b09119796c7b5274c04803c9c7372cf2c0e3c0bb
Matthew Major <mrmatias1997@gmail.com>
2022-10-13 00:56:26 -0500

    Used CSV data for products and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
 
+Product.destroy_all
+Category.destroy_all
 
-for a in 1..676 do
-  products = Product.create(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.between(from: 0, to: 250)
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+# If CSV was created by Excel in Windows you may also need to set an encoding type:
+# products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+
+  category = Category.find_or_create_by(
+    name: product["category"]
   )
+
+  # name,price,description,stock quantity,category
+
+  if category && category.valid?
+    category.product.create(
+      title:          product["name"],
+      description:    product["description"],
+      price:          product["price"],
+      stock_quantity: product["stock quantity"]
+    )
+
+  else
+    puts "Category is invalid #{product["category"]}"
+
+  end
+
 end
