0b2da51355ff04fcf96de5799dfabbae284b9f33
Matthew Major <mmajor2@academic.rrc.ca>
2022-10-12 19:50:10 -0500

    Updated routes to direct Products/ to controllers index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: "products#index"
+
   get 'products/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "products#index"
 end
